5012c261bdffc6551267a5bbdf66566e7c24c9d0
Pablo Pan Veira <pablopanveira@Mac.local>
2020-10-17 16:50:50 +0200

    Added welcome

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 #!/usr/bin/env node
 
-const clearConsole = require('clear-any-console');
 
-clearConsole();
+const pkgJSON = require('./package.json');
+const welcome = require('cli-welcome');
+
+welcome({
+    title:pkgJSON.name,
+    tagLine:`Nice to meet ya fella!`,
+    description:pkgJSON.description,
+    version:pkgJSON.version,
+    bgColor: `#FADC00`,
+    color: `#000000`,
+    bold: true,
+    clear: true
+
+})
+
+
 
 console.log(`
 Hello!
