a1e2b1e696ee002bd22ccd16a919b9ff3e9f9045
Pablo Pan Veira <pablopanveira@Mac.local>
2020-10-17 16:33:03 +0200

    Clear console + dependencies

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 #!/usr/bin/env node
 
+const clearConsole = require('clear-any-console');
+
+clearConsole();
+
 console.log(`
+Hello!
 Pablo Pan Veira - Developer & Team Leader
 
 Twitter: https://twitter.com/code4paul
