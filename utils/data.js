ead9603a3f341bab82a7ea6b98ea06a8d439722f
atxpaul <pablo.pan.veira@gmail.com>
2021-10-09 16:30:37 +0200

    Adding github social data

--- utils/data.js
+++ utils/data.js
@@ -1,25 +1,29 @@
 const chalk = require('chalk');
 
-
 const twitterClr = chalk.bgHex(`#1da1f2`).black;
+const githubClr = chalk.bgHex(`#171515`);
 const dim = chalk.dim;
 
-
 const social = `
-${twitterClr(` Twitter `)} ${dim(`https://twitter.com/code4paul`)}`
+${twitterClr(` Twitter `)} ${dim(`https://twitter.com/code4paul`)}
+${githubClr(` GitHub `)} ${dim(`https://github.com/atxpaul`)}`;
 const bio = `
 Hello fellas!
 
-${chalk.blue.underline(`Pablo Pan Veira`)} - ${chalk.bgYellow.black.bold(` Javascript Developer `)}
+${chalk.blue.underline(`Pablo Pan Veira`)} - ${chalk.bgYellow.black.bold(
+  ` Javascript Developer `
+)}
 
-${chalk.italic.yellow(`Project Leader | Javascript lover | Agile thinker | Rib cooker`)}`
+${chalk.italic.yellow(
+  `Project Leader | Javascript lover | Agile thinker | Rib cooker`
+)}`;
 
-const msg={
-    msg:`for checking my npm!`,
-    name:`Thanks`
-}
+const msg = {
+  msg: `for checking my npm!`,
+  name: `Thanks`,
+};
 module.exports = {
-    bio,
-    social,
-    msg
-}
+  bio,
+  social,
+  msg,
+};
